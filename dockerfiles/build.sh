510473eaf8318b7b7f5cb93a7f058a97daf834b0
ss75710541 <root@package1.10-3-10-33.crawler.ucloud-bj-bgp-c.dm>
2016-12-13 18:03:17 +0800

    add multicast discovery plugin

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 opts="--no-cache"
-docker build $opts -t library/centos7-jdk8-elasticsearch-2.4.1 .
+docker build $opts -f Dockerfile_runtime -t library/centos7-jdk8-elasticsearch-2.4.1 .
